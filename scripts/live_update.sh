c92d8ac2b2d9a99d679563bd065794627302d299
Joe Fox <joe.m.fox@gmail.com>
2026-03-16 10:51:36 -0400

    don't snapshot roster for live update

--- scripts/live_update.sh
+++ scripts/live_update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 DATE=$(date -d "yesterday 13:00" '+%Y-%m-%d')
 cd /opt/ballgame
-docker compose exec django python manage.py snapshot_roster $DATE
 docker compose exec django python manage.py realtime_update $DATE overwrite --workers 1
